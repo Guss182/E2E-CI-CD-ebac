02cff089f163142a9165945890a0adbbf536c0ab
Gustavo Moreno <gustavo_moreno.182@hotmail.com>
2026-02-09 13:01:37 -0300

    Alteração no workflow

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -4,14 +4,6 @@ import checkoutPage from '../support/page_objects/checkout.page'
 
 context('Exercicio - E2E - Fluxo de pedido', () => {
 
-    /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
-
     beforeEach(() => {
         cy.visit('minha-conta')
         cy.fixture('perfil').then((perfil) => {
